ac02577040b2cb66fc66d460686686ac94d09f87
Andrei Datcu <49794799+datcuandrei@users.noreply.github.com>
2022-02-19 00:34:38 +0200

    added src for version `1.2`
    - added functions for initializing the connection to the X server and to destroy it as well
    - minor changes to the version information page
    - removed debug info

--- xsdump.cc
+++ xsdump.cc
@@ -17,7 +17,6 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 #include <iostream>
-#include <string.h>
 
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
@@ -29,48 +28,26 @@
 #include <png++/rgb_pixel.hpp>
 #include <png++/types.hpp>
 
-#define VERSION_NUMBER 1.1
+#define VERSION_NUMBER 1.2
 
 Display *dpy;
 Screen *screen;
 int scr;
 XImage *img;
 
-typedef struct screenSize
+struct screenSize
 {
     int width;
     int height;
-}res;
+};
 
-void help()
-{
-    std::cout << "Usage: xsdump OPTION" << std::endl;
-    std::cout << std::endl;
-    std::cout << "Options are: " << std::endl;
-    std::cout << std::endl;
-    std::cout << "-h or --help: shows this help menu" << std::endl;
-    std::cout << "-v or --version: shows xsdump's version information" << std::endl;
-}
+void init_x();
+void destroy_x();
 
 int main(int argc, char *argv[])
 {
-    // connecting to xserver display
-    dpy = XOpenDisplay(NULL);
-
-    // in case there is no server
-    if(dpy == NULL)
-    {
-        fprintf(stderr, "[ERROR] Can't connect to XServer display.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    // setting the default screen
-    scr = DefaultScreen(dpy);
-    screen = XDefaultScreenOfDisplay(dpy);
-    
-
+    init_x();
     screenSize res = {XWidthOfScreen(screen), XHeightOfScreen(screen)}; // getting the resolution of the screen
-
     img = XGetImage(dpy,screen->root,0,0,res.width,res.height,AllPlanes,ZPixmap); // grabbing a screen dump of the whole screen (all displays)
     
     // rgb masks
@@ -103,41 +80,54 @@ int main(int argc, char *argv[])
         }
     }
 
-    std::string output;
+    std::string output; // the destination of the file will be stored here
     if(argc >= 2)
     {
+        // helpful commands for the users
         if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
         {
             fprintf(stdout,"Usage: xsdump OPTION\n\nOptions are:\n\n-o or --output [PATH]: outputs the image to PATH(include name of file & extension must be .png), otherwise is saved in HOME\n-h or --help: shows this help menu\n-v or --version: shows xsdump's version information");
         }else if(strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0)
         {
-            fprintf(stdout,"xsdump %g\nA very simple screenshot tool for X11.\nCopyright (C) 2022 Andrei Datcu.\nLicense GPLv2: <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>\nThis is free software: you are free to change and redistribute it.\nThere is NO WARRANTY, to the extent permitted by law.\n\nWritten by Andrei Datcu.", VERSION_NUMBER);
-        }else if(strcmp(argv[1], "-o") == 0 || strcmp(argv[1], "--output") == 0)
+            fprintf(stdout,"xsdump %g\nA very simple screenshot tool for X11.\nCopyright (C) 2022 Andrei Datcu <https://datcuandrei.github.io>.\nLicensed under GPL v2: <https://github.com/datcuandrei/xsdump/blob/main/LICENSE>\nVisit <https://github.com/datcuandrei/xsdump> for more information.", VERSION_NUMBER);
+        }else if(strcmp(argv[1], "-o") == 0 || strcmp(argv[1], "--output") == 0) // a way for the user to save the screen dump where he wants it.
         {
             output = argv[2];
             image.write(output);
-            std::cout << "[SUCCESS] Screen dumped!";
+            fprintf(stdout,"[SUCCESS] Screen dumped!");
         }    
     }else
     {
+        // by default, it will be saved in the home directory of the current user.
         output = getenv("HOME");
         output.append("/xsdump-dump.png");
         image.write(output);
-        std::cout << "[SUCCESS] Screen dumped!";
+        fprintf(stdout,"[SUCCESS] Screen dumped!");
     }
 
-    // debugging gibberish
-    /*std::cout << std::endl;
-    std::cout << "[DEBUG]" << std::endl;
-    std::cout << "RMask " << "GMask " << "BMask " << std::endl;
-    std::cout << "----- " << "----- " << "----- " << std::endl;
-    std::cout << rm << " " << gm << " " << bm << std::endl;
-    std::cout << "Resolution: " << res.width << "x" << res.height << std::endl;
-    std::cout << "Root Window ID: " << screen->root << std::endl;
-    std::cout << "Display: " << dpy << std::endl;
-    std::cout << std::endl;
-    std::cout << "[SUCCESS] Screen dumped!";*/
-
+    destroy_x();
     exit(EXIT_SUCCESS);
 }
-// end
+
+void init_x()
+{
+    // connecting to xserver display
+    dpy = XOpenDisplay(NULL);
+
+    // in case there is no server
+    if(dpy == NULL)
+    {
+        fprintf(stderr, "[ERROR] Can't connect to XServer display.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // setting the default screen
+    scr = DefaultScreen(dpy);
+    screen = XDefaultScreenOfDisplay(dpy);
+}
+
+void destroy_x()
+{
+    XDestroyImage(img); // destroying the image connection
+    XCloseDisplay(dpy); // closing the connection to the X server.
+}
